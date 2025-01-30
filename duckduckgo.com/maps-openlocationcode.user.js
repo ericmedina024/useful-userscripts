ba66af9b6120283e9bf93eab77fc3c0c279d9b51
Eric Medina <53099692+ericmedina024@users.noreply.github.com>
2025-01-29 22:13:29 -0600

    Add support for addresses

--- duckduckgo.com/maps-openlocationcode.user.js
+++ duckduckgo.com/maps-openlocationcode.user.js
@@ -4,7 +4,7 @@
 // @match       https://duckduckgo.com/*
 // @require     https://cdn.jsdelivr.net/openlocationcode/latest/openlocationcode.min.js
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Eric Medina
 // @description Adds OpenLocationCodes to the DuckDuckGo map address field
 // ==/UserScript==
@@ -12,6 +12,34 @@
 (async () => {
 
   const nativeFetch = window.fetch;
+
+  function morphPlacesResponse(responseBody) {
+    if ("results" in responseBody) {
+      for (let result of responseBody.results) {
+        if ("address" in result && "address_lines" in result) {
+          const locationCode = OpenLocationCode.encode(result.coordinates.latitude, result.coordinates.longitude, OpenLocationCode.CODE_PRECISION_EXTRA);
+          result["address"] += " ";
+          result["address"] += locationCode;
+          result["address_lines"].push(locationCode);
+        }
+      }
+    }
+  }
+
+  function morphFeaturesResponse(responseBody) {
+    if ("features" in responseBody) {
+      for (let feature of responseBody.features) {
+        if ("center" in feature && "context" in feature) {
+          const locationCode = OpenLocationCode.encode(feature.center[1], feature.center[0], OpenLocationCode.CODE_PRECISION_EXTRA);
+          for (let contextElement of feature.context) {
+            if (contextElement.id.includes("region") || contextElement.id.includes("country")) {
+              contextElement.text += ` ${locationCode}`;
+            }
+          }
+        }
+      }
+    }
+  }
 
   window.fetch = async function() {
     const response = await nativeFetch(...arguments);
@@ -25,19 +53,11 @@
     } catch (error) {
       return response;
     }
-    if ("results" in responseBody) {
-      for (let result of responseBody.results) {
-        if ("address" in result && "address_lines" in result) {
-          const locationCode = OpenLocationCode.encode(result.coordinates.latitude, result.coordinates.longitude, OpenLocationCode.CODE_PRECISION_EXTRA);
-          result["address"] += " ";
-          result["address"] += locationCode;
-          result["address_lines"].push(locationCode);
-        }
-      }
-    }
+    morphPlacesResponse(responseBody);
+    morphFeaturesResponse(responseBody);
     const freshResponse = await nativeFetch(...arguments);
     freshResponse.json = async () => responseBody;
     return freshResponse;
   }
-  
+
 })();
